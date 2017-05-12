572abe96f001347f53b3f13a1e3ae07b4d79b23a
sharon smith <sharon.e.smith2@gmail.com>
2017-05-12 11:16:07 -0500

    commiting working code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,35 @@
 
-console.log("writing to console");
+var app = angular.module("callanFoods", []);
+
+app.controller("FoodCtrl", ($scope) => {
+
+$scope.FoodItems = () => {
+
+		$scope.showGolden = false;
+		$scope.showMonster = false;
+		$scope.showRice = false;
+	};
+
+	$scope.burgerArt = () => {
+		$scope.showGolden = true;
+		$scope.showMonster = false;
+	};
+
+
+	// not working?
+	$scope.monsterArt = () => {
+		$scope.showMonster = true;
+		$scope.showGolden = false;
+		$scope.showRice = false;
+	};
+
+
+	// not working?
+	$scope.riceArt = () => {
+		$scope.showRice = true;
+		$scope.showMonster = false;
+		$scope.showGolden = false;
+	};
+
+
+});
